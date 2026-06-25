30b1f3964e8c478c3bb30ee1a1b82f01767f9665
SHANI SHARMA <shanisharma.fzd90@gmail.com>
2026-06-25 17:12:36 +0000

    Sync LeetCode submission Runtime - 8 ms (71.12%), Memory - 47.6 MB (63.05%)

--- 0303-range-sum-query---immutable/solution.java
+++ 0303-range-sum-query---immutable/solution.java
@@ -1,17 +1,16 @@
 class NumArray {
-           int[] pre;
+    int[] arr;
     public NumArray(int[] nums) {
-        int n = nums.length;
-        pre = new int[n];
-        pre[0] = nums[0];
-        for(int i=1;i<n;i++){
-           pre[i] = pre[i-1] + nums[i];
-        }
+       for( int i = 1 ; i < nums.length ; i++ ){
+            nums[i] += nums[i-1];
+       } 
+       arr = nums;
     }
+    
     public int sumRange(int left, int right) {
-          if(left==0)
-          return pre[right];
-          return pre[right]-pre[left - 1];
+      if ( left == 0 ) return arr[right];
+
+      return arr[right] - arr[left-1];
     }
 }
 
